df3a06caf13cca329331daf151da770e5e195299
t1man5577 <tjurayev52@gmail.com>
2023-01-31 11:23:13 +0500

    first commit

---
+++ alert.js
@@ -0,0 +1,20 @@
+// alert(
+//     "hammaga salom"
+
+// let name;
+// name = "John";
+// admin = name;
+// alert(admin);
+
+// let ourPlanetName = "Eart";
+// let currentUserName = "John";
+
+// const BIRTHDAY = '02.08.2002';
+// const AGE = someCode(BIRTDAY);
+// let test = prompt("Test", 'name');
+
+
+// let name = prompt("what is your name?", "");
+// alert(name);
+
+
---
+++ script.js
@@ -0,0 +1,37 @@
+// let a=12;
+// let b=3;
+// alert(a>=0)
+// alert(b<-2)
+
+// let a=2;
+// let b=4;
+// let c=22;
+// alert(a<=b<=c)
+
+
+// let a=1;
+// let b=2;
+// let c=3;
+// alert(a<b<c)
+
+
+// let a=2;
+// let b=3;
+// alert(a %2==2)
+// alert(b %2!=2)
+
+
+
+// let a=1
+// let b=12
+// alert(a%2==2)
+// alert(b%2!=2)
+
+
+
+
+
+
+// let a=12
+// let b=10
+ 
